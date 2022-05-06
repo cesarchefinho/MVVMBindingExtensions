69b3f40c56b94555a05df07d99f45fd3b8a23bf9
cesarchefinho <55895254+cesarchefinho@users.noreply.github.com>
2022-05-06 19:52:56 -0300

    Add files via upload

---
+++ Sample/LoginPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Hosting;
+using MonetizeLibrary;
+
+namespace Monetize
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class LoginPage : Page
+    {
+        public LoginViewModel ViewModel { get; set; } = new LoginViewModel();
+
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Sample/LoginViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using ComponetModel.DataAnnotaions;
+using CommunityToolkit.Mvvm;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+    public partial class LoginViewModel : MVVMBaseViewModel
+    {       
+   	[Display(Name = "User Id", Description = "Your UserId in format of 999.999.999-99")]
+        [DisplayFormat(DataFormatString = "999.999.999-99")]
+        [NotEmpty(ErrorMessage = "UserId is required", EmptyValue = "   .   .   -  ")]
+        [StringLength(maximumLength: 14, MinimumLength = 11, ErrorMessage = "Need between 11 or 14 chars!")]
+        // [UserId] => Some validatorAttribute
+        [ObservableProperty]        
+        private string userId = String.Empty;
+
+       [Display(Name = "Password", Description = "Type here your Passowrd")]
+       [DataType(DataType.Password)]
+       [Required(ErrorMessage = "The Password is required!")]
+       [StringLength(maximumLength: 16, MinimumLength = 6, ErrorMessage = "minimum of 6 and maximum of 16 chars !")]
+       [Password]
+       [ObservableProperty]
+       private string password = String.Empty;
+ 
+       [Display(Name = "Newsletter", Description = "Marque esta opção indicando que você aceita receber nossos emails.")]
+       [IsCheckedAttribute ("It is required accept receive emails to Login.")]
+       [ObservableProperty]
+       private bool newsletter;
+
+       [ICommand]
+       private async Task Login()
+       {
+          ValidateAll ();
+
+          if (!Usuario.HasErrors)
+          {
+             // Do Something
+          }
+    }
+
